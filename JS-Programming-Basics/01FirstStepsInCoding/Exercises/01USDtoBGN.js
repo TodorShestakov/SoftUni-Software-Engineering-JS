cb0e6502f6fca7876cae819d2a16eb900ca0ef7d
Todor Shestakov <to.shestakov@gmail.com>
2022-04-01 14:21:33 +0300

    Rename folder

--- JS-Programming-Basics/01FirstStepsInCoding/Exercises/01USDtoBGN.js
+++
@@ -1,9 +0,0 @@
-function data(input) {
-
-    let usd = Number(input[0]);
-    let bgn = 1.79549 * usd;
-
-    console.log(bgn);
-
-}
-data(["22"]);
--- JS-Programming-Basics/01FirstStepsInCoding/Lab/03SquareArea.js
+++
@@ -1,7 +0,0 @@
-function squareArea(input) {
-
-    let a = Number(input[0]);
-    let result = a * a;
-    console.log(result);
-}
-squareArea(["5"]);
