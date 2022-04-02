7a3fc4bd81b42836614d2eda102400d00f5b1dbf
Todor Shestakov <to.shestakov@gmail.com>
2022-04-02 18:50:08 +0300

    Added new projects

---
+++ JS-Programming-Basics/04.Loops/Exercise/01NumbersEndingIn7.js
@@ -0,0 +1,11 @@
+function ending(input) {
+
+    for (let currNum = 1; currNum <= 1000; currNum++) {
+
+        if (currNum % 10 === 7) {
+            console.log(currNum);
+        }
+    }
+
+}
+ending()
