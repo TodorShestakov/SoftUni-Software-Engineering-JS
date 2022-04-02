7209f9fd45d01a7b06d16fa25b061a64d12480f6
TodorShestakov <102318727+TodorShestakov@users.noreply.github.com>
2022-04-02 13:21:41 +0300

    Update 04VacationBooksList.js

--- JS-Programming-Basics/01FirstStepsInCoding/Exercises/04VacationBooksList.js
+++ JS-Programming-Basics/01FirstStepsInCoding/Exercises/04VacationBooksList.js
@@ -1,9 +1,4 @@
 function data(input){
-
-    // 1.	Брой страници в текущата книга – цяло число в интервала [1…1000];
-    // 2.	Страници, които може да прочита за 1 час – цяло число в интервала [1…1000];
-    // 3.	Броя на дните, за които трябва да прочете книгата – цяло число в интервала [1…1000];
-    
     let countPages = Number(input[0]);
     let pagesForHour = Number(input[1]);
     let countDaysToReadTheBook = Number(input[2]);
@@ -12,8 +7,6 @@ function data(input){
     let hoursToFinish = hoursForOneBook / countDaysToReadTheBook;
 
     console.log(hoursToFinish);
-
-
 }
 data(["212",
 "20",
