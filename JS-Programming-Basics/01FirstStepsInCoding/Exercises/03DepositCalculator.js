c3fa0d5c27b72fd29d2d3ec6a8b2dde4a3e0afc9
TodorShestakov <102318727+TodorShestakov@users.noreply.github.com>
2022-04-02 13:21:08 +0300

    Update 03DepositCalculator.js

--- JS-Programming-Basics/01FirstStepsInCoding/Exercises/03DepositCalculator.js
+++ JS-Programming-Basics/01FirstStepsInCoding/Exercises/03DepositCalculator.js
@@ -1,9 +1,5 @@
 function data(input){
 
-// 1.	Депозирана сума – реално число в интервала [0.00 … 10000.00];
-// 2.	Срок на депозита(в месеци) – цяло число[1..12];
-// 3.	Годишен лихвен процент – реално число в интервала [0.00 … 10000.00];
-
 let depositedSum = Number(input[0]);
 let timeMonths = Number(input[1]);
 let annualPerc = Number(input[2]);
