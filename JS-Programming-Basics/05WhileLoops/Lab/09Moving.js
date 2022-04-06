e50240709c49f0bd4ff3438040cae495704a4384
Todor Shestakov <to.shestakov@gmail.com>
2022-04-06 11:50:18 +0300

    Solved Moving task

---
+++ JS-Programming-Basics/05WhileLoops/Lab/09Moving.js
@@ -0,0 +1,29 @@
+function balance(input) {
+
+    let w = Number(input[0]);
+    let l = Number(input[1]);
+    let h = Number(input[2]);
+
+    let volume = w * l * h;
+    let index = 3;
+    let sum = 0;
+    let command = input[index];
+
+    while (command !== 'Done') {
+
+        sum += Number(command);
+        index++;
+
+        if (sum > volume) {
+            break;
+        }
+        command = input[index];
+    }
+    if (sum > volume) {
+        console.log(`No more free space! You need ${sum - volume} Cubic meters more.`);
+    } else {
+        console.log(`${volume - sum} Cubic meters left.`);
+    }
+}
+balance(["10", "1", "2", "4", "6", "Done"])
+
