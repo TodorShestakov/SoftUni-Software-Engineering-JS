7588823fcf0845020916d51233942b876a394fb1
Todor Shestakov <to.shestakov@gmail.com>
2022-04-13 13:46:45 +0300

    Solved Divisible by 3 task

---
+++ JS-Fundamentals/01.JS-SyntaxConditionalStatementsAndLoops/Lab/08DivisibleBy3.js
@@ -0,0 +1,16 @@
+function solve() {
+
+    /* for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0) {
+            console.log(i);
+        }
+
+    } */
+
+    let n = 3;
+    while (n <= 100) {
+        console.log(n);
+        n += 3;
+    }
+}
+solve()
