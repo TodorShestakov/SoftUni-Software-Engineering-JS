62aaa60a063d7f37be4b8e4aa8e217ecfbb459fb
Todor Shestakov <to.shestakov@gmail.com>
2022-04-28 10:55:27 +0300

    Solved Lower or Upper task

---
+++ JS-Fundamentals/02DataTypesAndVariables/Exercise/07LowerOrUpper.js
@@ -0,0 +1,10 @@
+function solve(inputChar) {
+
+    if (inputChar === inputChar.toUpperCase()) {
+        console.log("upper-case");
+    } else if (inputChar === inputChar.toLowerCase()) {
+        console.log("lower-case");
+    }
+}
+solve('L');
+solve('f');
