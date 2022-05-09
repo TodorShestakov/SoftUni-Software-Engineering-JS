a273cc0cfa4c2a5107ef36528db903fd618505d6
Todor Shestakov <to.shestakov@gmail.com>
2022-05-09 15:01:15 +0300

    Solved Reverse in Place task

---
+++ JS-Fundamentals/03Arrays/Lab/04ReverseInPlace.js
@@ -0,0 +1,11 @@
+function inPlace(arr) {
+
+    for (let i = 0; i < arr.length / 2; i++) {
+        let k = arr.length - 1 - i;
+        let temp = arr[i];
+        arr[i] = arr[k];
+        arr[k] = temp;
+    }
+    console.log(arr.join(' '));
+}
+inPlace(['abc', 'def', 'hig', 'klm', 'nop']);
