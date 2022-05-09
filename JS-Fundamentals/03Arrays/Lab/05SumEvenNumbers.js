ec38ef1284ee99aa9682f016f14180f6ee97f478
Todor Shestakov <to.shestakov@gmail.com>
2022-05-09 17:38:36 +0300

    Solved Sum Even Numbers task

---
+++ JS-Fundamentals/03Arrays/Lab/05SumEvenNumbers.js
@@ -0,0 +1,19 @@
+function even(arr) {
+
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+
+        arr[i] = Number(arr[i]);
+    }
+
+    for (let num of arr) {
+        if (num % 2 === 0) {
+            sum += num;
+        }
+    }
+    
+console.log(sum);
+
+}
+even(['2','4','6','8','10']);
