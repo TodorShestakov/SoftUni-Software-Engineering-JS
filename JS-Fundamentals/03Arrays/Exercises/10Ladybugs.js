2ecc3816db44427504b3583d2f4b0852da964eb4
Todor Shestakov <to.shestakov@gmail.com>
2022-05-12 16:19:19 +0300

    Solved Ladybugs task

---
+++ JS-Fundamentals/03Arrays/Exercises/10Ladybugs.js
@@ -0,0 +1,39 @@
+function ladyBugs(input) {
+
+    let n = input.shift(); 
+    let field = (new Array(n)).fill(0); 
+    let ladyBugIndexes = input.shift().split(' ');
+    for (let index of ladyBugIndexes) {
+        index = Number(index);
+        if (index >= 0 && index < n) {
+            field[index] = 1;
+        }
+    }
+    for (let command of input) {
+
+        let tokens = command.split(' '); 
+        let startingIndex = Number(tokens[0])
+        if (field[startingIndex] == 1) {
+            let currentIndex = startingIndex;
+            let direction = tokens[1]; 
+            let offset = Number(tokens[2]); 
+            if (direction == 'left') {
+                offset *= -1;
+            }
+
+            while (currentIndex >= 0 && currentIndex < n && field[currentIndex] == 1) {
+                currentIndex += offset;
+            }
+            field[startingIndex] = 0;
+            if (currentIndex >= 0 && currentIndex < n ) {
+                field[currentIndex] = 1;
+            }
+        }
+    }
+
+    console.log(field.join(' '));
+}
+ladyBugs([ 5, '3',
+'3 left 2',
+'1 left -2']
+);
