fb11bea817997afa4e6e00ec99f567382ee447ea
Todor Shestakov <to.shestakov@gmail.com>
2022-05-19 14:02:06 +0300

    Solved Print Certificate task

---
+++ JS-Fundamentals/04Functions/Lab/PrintCertificate.js
@@ -0,0 +1,49 @@
+function printCertificate(grade, names) {
+    if (pass(grade)) {
+        printHeader();
+        printName(names);
+        formatGrade(grade);
+    } else {
+        console.log('Student does not qualify');
+    }
+}
+
+function printHeader() {
+    console.log('~~~-   {@}   -~~~');
+    console.log('~- Certificate -~');
+    console.log('~~~-  ~---~  -~~~');
+}
+
+function printName(nameArr) {
+    console.log(nameArr[0] + ' ' + nameArr[1]);
+}
+
+function formatGrade(grade) {
+    let formatGrade = grade.toFixed(2);
+    let desc;
+
+    if (grade < 3) {
+        formatGrade = '2';
+        desc = 'Fail'
+    } else if (grade < 3.5) {
+        desc = 'Poor'
+    } else if (grade < 4.5) {
+        desc = 'Good'
+    } else if (grade < 5.5) {
+        desc = 'Very good'
+    } else {
+        desc = 'Excellent'
+    }
+
+    console.log(`${desc} (${formatGrade})`);
+}
+
+function pass(grade) {
+    return grade >= 3;
+}
+
+
+
+printCertificate(5.256, ['John', 'Smith'])
+printCertificate(4.30, ['Peter', 'Roberts'])
+printCertificate(2.80, ['Geroge', 'Bobson'])
