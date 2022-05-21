ca087d7dd48f5743c87a2f6706e18d493bab724d
Todor Shestakov <to.shestakov@gmail.com>
2022-05-21 18:22:57 +0300

    Solved NxN Matrix task

---
+++ JS-Fundamentals/04Functions/Exercise/07NxNMatrix.js
@@ -0,0 +1,12 @@
+function nxnMatrix(num) {
+
+    for (let i = 0; i < num; i++) {
+        let result = '';
+
+        for (let j = 0; j < num; j++) {
+            result += `${num} `
+        }
+        console.log(result);
+    }
+}
+nxnMatrix(3);
