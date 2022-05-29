20e312dd9d5d1f61f61946662008e4e9af550892
Todor Shestakov <to.shestakov@gmail.com>
2022-05-29 16:48:02 +0300

    Solved Last K Numbers Sequance task

---
+++ JS-Fundamentals/05.AdvancedArrays/Lab/04LastKNumbersSequance.js
@@ -0,0 +1,16 @@
+function kSequence(n, k) {
+
+    let result = [1];
+
+    for (let i = 0; i < n - 1; i++) {
+        let lastK = result.slice(-k);
+
+        let sum = 0;
+        for (let element of lastK) {
+            sum += element;
+        }
+        result.push(sum);
+    }
+    console.log(result.join(' '));
+}
+kSequence(8, 2);
